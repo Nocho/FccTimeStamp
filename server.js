3e569e8ffcb4b6de56960abe4c8e651224c8334e
Nicolas Ochoa <nic.d.ochoa@gmail.com>
2017-02-11 00:56:29 +0000

    added server.js logic

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+// always port 8080
 const express = require('express');
+const url = require('url');
+const http = require('http'); // should this be https? 
+const url = require('url');
+
+const app = express();
+const server = http.createServer(function(req, res){
+    //req -> http.IncomingMessage
+    var urlo = url.parse(req.url); // parsed http URL
+    res.send(urlo);
+    res.end();
+    
+})
